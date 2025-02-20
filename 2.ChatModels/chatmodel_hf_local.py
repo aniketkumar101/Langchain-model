5578ec7ed8fbcdfca23fac14137c45cd4a72e45e
aniket kumar <aniket.787068@gmail.com>
2025-02-21 01:20:42 +0530

    chat model of hf local

--- 2.ChatModels/chatmodel_hf_local.py
+++ 2.ChatModels/chatmodel_hf_local.py
@@ -5,13 +5,13 @@ llm = HuggingFacePipeline.from_model_id(
     model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
     task="text-generation",
     pipeline_kwargs=dict(
-        temperature=0.5,
-        max_new_token=100
+        temperature=0.5
+        # max_new_token=10
     )
 )
 
 model = ChatHuggingFace(llm=llm)
 
-result = model.invoke("what is the capital of India")
+result = model.invoke("who is prime minister of india")
 
 print(result.content)
